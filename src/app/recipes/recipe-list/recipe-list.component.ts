7e0c242ce108b5dda8149dd86cc458c2354954f6
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-09-25 23:55:29 +0200

    add test recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes = [
+    new Recipe(
+      'A test recipe',
+      'this is a simply test recipe',
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
+    ),
+  ];
 
   constructor() {}
 
