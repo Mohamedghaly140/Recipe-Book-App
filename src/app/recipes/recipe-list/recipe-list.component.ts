913cb7486fd7cc764f6456efc96d72da7c35411f
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-09-26 01:01:18 +0200

    add reacipe list using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'this is a simply test recipe',
       'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
     ),
+    new Recipe(
+      'A test recipe',
+      'this is a simply test recipe',
+      'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'
+    ),
   ];
 
   constructor() {}
